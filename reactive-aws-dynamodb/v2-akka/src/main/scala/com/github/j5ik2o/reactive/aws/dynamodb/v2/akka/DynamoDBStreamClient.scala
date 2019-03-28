1094451230b9a0ace0319ee87a7d1e5b2b67130e
Junichi Kato <j5ik2o@gmail.com>
2019-03-28 15:21:40 +0900

    move logic to trait.

--- reactive-aws-dynamodb/v2-akka/src/main/scala/com/github/j5ik2o/reactive/aws/dynamodb/v2/akka/DynamoDBStreamClient.scala
+++ reactive-aws-dynamodb/v2-akka/src/main/scala/com/github/j5ik2o/reactive/aws/dynamodb/v2/akka/DynamoDBStreamClient.scala
@@ -2,8 +2,8 @@
 package com.github.j5ik2o.reactive.aws.dynamodb.v2.akka
 
 import akka.NotUsed
-import akka.stream.scaladsl.Flow
-import com.github.j5ik2o.reactive.aws.dynamodb.akka
+import akka.stream.scaladsl.{ Flow, Source }
+import com.github.j5ik2o.reactive.aws.dynamodb.akka.{ DynamoDBStreamClient => BaseDynamoDBStreamClient }
 import com.github.j5ik2o.reactive.aws.dynamodb.model._
 import com.github.j5ik2o.reactive.aws.dynamodb.v2.DynamoDBAsyncClient
 
@@ -13,14 +13,24 @@ object DynamoDBStreamClient {
 
 }
 
-trait DynamoDBStreamClient extends akka.DynamoDBStreamClient with DynamoDBStreamClientSupport {
+trait DynamoDBStreamClient extends BaseDynamoDBStreamClient with DynamoDBStreamClientSupport {
+  override val underlying: DynamoDBAsyncClient
+  def batchGetItemFlow: Flow[BatchGetItemRequest, BatchGetItemResponse, NotUsed] =
+    Flow[BatchGetItemRequest].flatMapConcat { request =>
+      Source.fromPublisher(underlying.batchGetItemPaginator(request))
+    }
 
-  def batchGetItemFlow: Flow[BatchGetItemRequest, BatchGetItemResponse, NotUsed]
+  def listTablesFlow: Flow[ListTablesRequest, ListTablesResponse, NotUsed] = Flow[ListTablesRequest].flatMapConcat {
+    request =>
+      Source.fromPublisher(underlying.listTablesPaginator(request))
+  }
 
-  def listTablesFlow: Flow[ListTablesRequest, ListTablesResponse, NotUsed]
+  def queryFlow: Flow[QueryRequest, QueryResponse, NotUsed] = Flow[QueryRequest].flatMapConcat { request =>
+    Source.fromPublisher(underlying.queryPaginator(request))
+  }
 
-  def queryFlow: Flow[QueryRequest, QueryResponse, NotUsed]
-
-  def scanFlow: Flow[ScanRequest, ScanResponse, NotUsed]
+  def scanFlow: Flow[ScanRequest, ScanResponse, NotUsed] = Flow[ScanRequest].flatMapConcat { request =>
+    Source.fromPublisher(underlying.scanPaginator(request))
+  }
 
 }
