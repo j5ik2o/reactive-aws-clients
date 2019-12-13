faa89b6ac0c411c1ee64e60edffd3afc6350b6e1
Junichi Kato <j5ik2o@gmail.com>
2019-12-13 15:43:05 +0900

    Update ToAllOps trait

--- reactive-aws-eks/core/src/main/scala/com/github/j5ik2o/reactive/aws/eks/model/ops/ToAllOps.scala
+++ reactive-aws-eks/core/src/main/scala/com/github/j5ik2o/reactive/aws/eks/model/ops/ToAllOps.scala
@@ -1,24 +1,36 @@
 package com.github.j5ik2o.reactive.aws.eks.model.ops
 
+import com.github.j5ik2o.reactive.aws.utils.ToOptionSupport
+
 trait ToAllOps
-    extends ToCertificateOps
-    with ToClusterOps
-    with ToCreateClusterRequestOps
-    with ToCreateClusterResponseOps
-    with ToDeleteClusterRequestOps
-    with ToDeleteClusterResponseOps
-    with ToDescribeClusterRequestOps
-    with ToDescribeClusterResponseOps
-    with ToDescribeUpdateRequestOps
-    with ToDescribeUpdateResponseOps
-    with ToErrorDetailOps
-    with ToListClustersRequestOps
-    with ToListClustersResponseOps
-    with ToListUpdatesRequestOps
-    with ToListUpdatesResponseOps
-    with ToUpdateClusterVersionRequestOps
-    with ToUpdateClusterVersionResponseOps
-    with ToUpdateOps
-    with ToUpdateParamOps
-    with ToVpcConfigRequestOps
-    with ToVpcConfigResponseOps
+extends ToOptionSupport with ToCertificateOps
+ with ToClusterOps
+ with ToCreateClusterRequestOps
+ with ToCreateClusterResponseOps
+ with ToDeleteClusterRequestOps
+ with ToDeleteClusterResponseOps
+ with ToDescribeClusterRequestOps
+ with ToDescribeClusterResponseOps
+ with ToDescribeUpdateRequestOps
+ with ToDescribeUpdateResponseOps
+ with ToErrorDetailOps
+ with ToIdentityOps
+ with ToListClustersRequestOps
+ with ToListClustersResponseOps
+ with ToListTagsForResourceRequestOps
+ with ToListTagsForResourceResponseOps
+ with ToListUpdatesRequestOps
+ with ToListUpdatesResponseOps
+ with ToLogSetupOps
+ with ToLoggingOps
+ with ToOIDCOps
+ with ToTagResourceRequestOps
+ with ToUntagResourceRequestOps
+ with ToUpdateClusterConfigRequestOps
+ with ToUpdateClusterConfigResponseOps
+ with ToUpdateClusterVersionRequestOps
+ with ToUpdateClusterVersionResponseOps
+ with ToUpdateOps
+ with ToUpdateParamOps
+ with ToVpcConfigRequestOps
+ with ToVpcConfigResponseOps
