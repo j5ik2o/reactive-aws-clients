d25e791c278660203a4ad21726a811ff1122ac36
Scala Steward <me@scala-steward.org>
2019-12-23 22:27:23 +0100

    Update appsync, batch, cloudformation, ... to 2.10.41

--- project/Settings.scala
+++ project/Settings.scala
@@ -12,7 +12,7 @@ object Settings {
   val scalaVersion212 = "2.12.10"
   val scalaVersion213 = "2.13.1"
 
-  val awsSdk2Version = "2.10.37"
+  val awsSdk2Version = "2.10.41"
 
   val catsVersion       = "2.0.0"
   val catsEffectVersion = "2.0.0"
