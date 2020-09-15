182c0a0974fcb0f67e60b1a79fe5356952eaef4a
Scala Steward <me@scala-steward.org>
2020-09-15 06:29:33 +0200

    Update DynamoDBLocal to 1.13.4

--- project/Settings.scala
+++ project/Settings.scala
@@ -150,7 +150,7 @@ object Settings {
 
   val coreWithTestSettings = coreSettings ++ testSettings
 
-  val dynamoDBLocalVersion = "1.13.3"
+  val dynamoDBLocalVersion = "1.13.4"
   val sqlite4javaVersion   = "1.0.392"
 
   lazy val copySqlite4javaJars = taskKey[Unit]("copyJars")
