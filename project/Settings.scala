5f99878b4a9f7015bd313b65057ff4fcabdb1389
Scala Steward <me@scala-steward.org>
2020-10-16 14:30:00 +0200

    Update akka-actor, akka-slf4j, akka-stream to 2.5.32

--- project/Settings.scala
+++ project/Settings.scala
@@ -17,7 +17,7 @@ object Settings {
   val catsVersion       = "2.0.0"
   val catsEffectVersion = "2.0.0"
   val monixVersion      = "3.1.0"
-  val akka25Version     = "2.5.30"
+  val akka25Version     = "2.5.32"
   val akka26Version     = "2.6.8"
 
   val compileScalaStyle = taskKey[Unit]("compileScalaStyle")
